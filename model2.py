5f7484b962f8364e219b057177d368d7097876e9
madankh <khadkasuman0000@gmail.com>
2024-06-17 14:27:54 +0545

    change in model

---
+++ model2.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.functional as F
+
+
+class Encoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class Decoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        
--- train.py
+++ train.py
@@ -1,6 +1,6 @@
 import torch
-import torchvision.datasets as datasets 
-# from tqdm import tqdm
+import torchvision.datasets as datasets
+from tqdm import tqdm
 import torch.nn as nn
 from model import VariationalAutoEncoder
 from torchvision.utils import save_image
@@ -19,5 +19,25 @@ LR_RATE = 3e-4 # learning rate
 dataset = datasets.MNIST(root="dataset/", train=True, transform=transforms.ToTensor(), download=True)
 train_loader = DataLoader(dataset=dataset, batch_size=BATCH_SIZE, shuffle=True)
 model = VariationalAutoEncoder(INPUT_DIM, H_DIM, Z_DIM).to(DEVICE)
-optimizer = torch.optim.AdamW(model.parameters(), LR_RATE)
+optimizer = torch.optim.AdamW(model.parameters(), lr=LR_RATE)
+loss_fn = torch.nn.BCELoss(reduction='sum')
 
+# Training loop
+for epoch in range(NUM_EPOCHS):
+    loop = tqdm(enumerate(train_loader), total=len(train_loader))
+    for i, (x, _) in loop:
+        x = x.to(DEVICE).view(x.shape[0], INPUT_DIM)
+        x_reconstructed, mu, sigma = model(x)
+        
+        # Compute loss
+        reconstruction_loss = loss_fn(x_reconstructed, x)
+        kl_div = -0.5 * torch.sum(1 + torch.log(sigma.pow(2)) - mu.pow(2) - sigma.pow(2))
+        loss = reconstruction_loss + kl_div
+        
+        # Backprop
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        loop.set_description(f"Epoch [{epoch+1}/{NUM_EPOCHS}]")
+        loop.set_postfix(loss=loss.item())
